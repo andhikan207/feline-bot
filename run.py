a1e2fce64c44e09385df73e9b665813a1a7d3788
andhikan207 <andhika.nwp2@gmail.com>
2025-02-25 14:58:34 +0700

    RESET WHOLE STUFF

---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+#load env vars
+load_dotenv()
+
+AUTHTKN = os.getenv("AUTH_TKN")
+
+intents = discord.Intents.defaulk()
+bot = commands.Bot(command_prefix = "!", intents = intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(AUTHTKN)
